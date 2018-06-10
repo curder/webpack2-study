5f0568d59899c5a47f72418018f1d90d4f630390
curder <curder@foxmail.com>
2018-06-10 13:23:13 +0800

    将项目文件切割成各自模块

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const PATHS = {
+    app: path.join(__dirname , 'app'),
+    dist: path.join(__dirname, 'dist'),
+}
+
+module.exports = {
+    entry: {
+        app: PATHS.app,
+    },
+    output: {
+        path: PATHS.dist,
+        filename: "[name].js",
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Webpack Demo",
+        }),
+    ]
+};
